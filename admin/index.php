ac260fc760b1021e13d8db36e525800fd9c457e4
ALFIKRI <alfikri.name@gmail.com>
2023-09-12 16:20:57 +0700

    set layout

---
+++ admin/index.php
@@ -0,0 +1,25 @@
+<?php
+$page = isset($_GET['page']) ? $_GET['page'] : 'home';
+switch ($page) {
+    case 'login':
+        include "./module/auth/login.php";
+        break;
+    case 'home':
+        include "home.php";
+        break;
+    case 'barang':
+        include "barang.php";
+        break;
+    case 'kategori':
+        include "kategori.php";
+        break;
+    case 'user':
+        include "user.php";
+        break;
+    case 'logout':
+        include "logout.php";
+        break;
+    default:
+        echo "<center><h3>Maaf. Halaman tidak di temukan !</h3></center>";
+        break;
+}
